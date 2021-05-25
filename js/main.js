766fa780fb683cdada584afa7f712c8c00c9c9e8
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-05-25 10:57:11 -0700

    Checkpoint 6.3

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
 /* global data */
 /* exported data */
 var $photoURL = document.querySelector('.photoURL');
-// var $button = document.querySelector('buttoned');
 
 $photoURL.addEventListener('input', function (event) {
   document.getElementById('placeholderImg').src = event.target.value;
 });
 
-// $button.addEventListener('submit' function (event) {
+// var $form = document.querySelector('#codeForm');
 
+// var formTitle = $form.title.value;
+// var formURL = $form.photoURL.value;
+// var formNotes = $form.notes.value;
+
+// $form.addEventListener('submit' function (event) {
+//   event.preventDefault();
+
+//   var formData = {
+//     title: formTitle,
+//     photoURL: formURL,
+//     notes: formNotes
+//   }
+//   formData.entryId = data.nextEntryId;
+//   data.nextEntryId++
 // })
