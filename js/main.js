8b27c3f8004322444b88947fd1602eac09dc17fd
Michael Aguilar <michaelmaguilar95@gmail.com>
2021-05-24 22:21:21 -0700

    Checkpoint, to 6 now.

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var $photoURL = document.querySelector('.photoURL');
 // var $button = document.querySelector('buttoned');
 
 $photoURL.addEventListener('input', function (event) {
-  $photoURL.setAttribute('src', event.target.value);
+  document.getElementById('placeholderImg').src = event.target.value;
 });
 
 // $button.addEventListener('submit' function (event) {
